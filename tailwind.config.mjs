cd7230d3f95cb1ea340be7d6783c768e375619cc
Luka <salevic@lu-ka.me>
2024-10-26 11:59:08 +0200

    Add default app-wide border styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,9 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			"borderColor": { "DEFAULT": "rgb(64 64 64 / 0.6)" },
+		},
 	},
 	plugins: [],
 }
